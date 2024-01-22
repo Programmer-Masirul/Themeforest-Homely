4f61eb94adb94b3b003f2d7ba2e988af00e37551
masirul-codify <masirulislam80@gmail.com>
2024-01-22 22:28:15 +0600

    add: all sections footer

--- js/custom.js
+++ js/custom.js
@@ -2,6 +2,39 @@
 (function ($) {
     "use strict";
     $(document).ready(function () {
+        
+//isotope.pkgd.js
+// init Isotope
+var $table = $('.table-like').isotope({
+    layoutMode: 'vertical',
+    getSortData: {
+      name: '.name',
+      symbol: '.symbol',
+      number: '.number parseInt',
+      category: '.category',
+      weight: function( itemElem ) {
+        var weight = $( itemElem ).find('.weight').text();
+        return parseFloat( weight.replace( /[\(\)]/g, '') );
+      }
+    }
+  });
+  
+  // bind sort button click
+  $('#sorts').on( 'click', 'button', function() {
+    var sortValue = $(this).attr('data-sort-value');
+    $table.isotope({ sortBy: sortValue });
+  });
+  
+  // change is-checked class on buttons
+  $('.button-group').each( function( i, buttonGroup ) {
+    var $buttonGroup = $( buttonGroup );
+    $buttonGroup.on( 'click', 'button', function() {
+      $buttonGroup.find('.is-checked').removeClass('is-checked');
+      $( this ).addClass('is-checked');
+    });
+  });
+  
+// isotop end
 
 
 
